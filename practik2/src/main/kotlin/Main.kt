89cb520d3ceb9786eddb04ea5428548774d7cbca
saidadanil228ayebasota <saida.askarova@internet.ru>
2022-10-13 14:24:50 +0300

    Добавил 2 практику

---
+++ practik2/src/main/kotlin/Main.kt
@@ -0,0 +1,41 @@
+fun main(args: Array<String>) {
+    /*
+      val age1: Int = 42
+      val age2: Int = 21
+
+      val age1: Int = 42
+      val age2: Int = 21
+      val avg1: Int = (age1 + age2) / 2
+
+      val age1: Double = 42.0
+      val age2: Double = 21.0
+      val avg1: Double = (age1 + age2) / 2
+      print(avg1)
+
+      val firstName :String =  "Sasha"
+      val lastName :String =  "Denisenko"
+
+      val firstName :String =  "Sasha"
+      val lastName :String =  "Denisenko"
+      val fullName :String =  firstName + " " + lastName
+      val myDetails :String =  "Привет, меня зовут"
+      println(myDetails + " " + fullName)
+
+
+      val dates = Triple(13,10,2015)
+
+      val day = dates.first
+      val month = dates.second
+      val year = dates.third
+      /*println(day)
+      println(month)
+      println(year)*/
+
+      val monthANDyear = """
+              |$month
+              |$year
+      """.trimMargin()
+      println(monthANDyear)
+      */
+    val pair = Pair (2017, year)
+}
