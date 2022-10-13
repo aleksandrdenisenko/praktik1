876218469437fa8a744d2d6446984ed273338aa7
saidadanil228ayebasota <saida.askarova@internet.ru>
2022-10-13 13:33:32 +0300

    first commit

---
+++ практическая работа-1/src/main/kotlin/Main.kt
@@ -0,0 +1,72 @@
+import kotlin.math.*
+
+fun main()
+{
+  val exercises: Int = 13;
+   var exercisesSolved: Int = 0;
+   exercisesSolved += 1;
+/*
+   val Age: Int = 17
+   var averageAge: Double = 18.0
+   averageAge = (averageAge + 30.0)/ 2.0
+   print(averageAge)
+
+   val testNumber : Int = 17
+   val even0dd : Int = testNumber % 2
+   print(even0dd)
+
+   var answer: Int = 0
+   answer += (1 + 10) * 10 shr 3
+   print(answer)
+
+   var age: Int = 16
+   println(age)
+   age = 30
+   print(age)
+
+   val a: Int = 46
+   val b: Int = 10
+   val answer1: Int = (a * 100) + b
+   val answer2: Int = (a * 100) + (b * 100)
+   val answer3: Int = (a * 100) + (b / 10)
+   println(answer1)
+   println(answer2)
+   println(answer3)
+
+    (5 * 3) - ((4 / 2) * 2)
+
+    val a: Double = 6.0
+    val b: Double = 17.0
+    val average: Double = a + b / 2
+
+    val fahrenheit: Double = 10.0
+    val celcius: Double = fahrenheit - 32.0 / 1.8
+    print(celcius)
+
+    val pozicion: Int = 36
+    val row: Int = pozicion / 8
+    val cul: Int = pozicion % 8
+    println(pozicion)
+    println(row)
+    println(cul)
+
+    val degrees: Double = 40.0
+    val radians: Double = PI * degrees / 180.0
+    println(radians)
+    exercisesSolved += 1
+
+   */
+    val x1: Double = 5.0
+    val x2: Double = 7.0
+    val y1: Double = 2.0
+    val y2: Double = 10.0
+    val distance: Double = sqrt((x2 - x1).pow(2)) + ((y2 - y2). pow(2))
+    println(distance)
+    exercisesSolved += 1
+    println(exercisesSolved)
+
+
+
+
+
+}
